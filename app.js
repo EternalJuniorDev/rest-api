a27ff3ec030b9323ebc0c42959382108fa7655dd
BadProgammer54 <puhat-lamer@mail.ru>
2020-06-06 12:38:08 +0700

    Make default server oprions

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
 const express = require('express')
+const path = require('path')
+const app = express();
+
+
+
+
+
+
+
+
+
+
+
+app.use(express.static(path.resolve(__dirname, 'clients')))
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'clients', 'index.html'))
+})
+
+app.listen(3000, () => console.log('Server has been started on port 3000...'))
